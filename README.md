ba532954b258c99166b7f1bc3b79cdc3385694ab
dymafr <erwan1309@hotmail.com>
2022-02-17 10:59:07 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-vite-ebzhfc
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-ebzhfc)
---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>Bonjour <span v-html="name"></span></h1>
+  <!-- Remarquez la différence avec v-text : -->
+  <div>{{ name }}</div>
+</template>
+
+<script setup lang="ts">
+const name = '<i>Jean</i>';
+</script>
+
+<style></style>
